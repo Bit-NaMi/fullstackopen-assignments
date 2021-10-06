a03078790c7f77cd9a879840193e9da803af510f
Minna P <60325864+Bit-NaMi@users.noreply.github.com>
2021-10-06 23:15:39 +0300

    Update App.js

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,20 +1,5 @@
 import React, {useState} from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
@@ -42,12 +27,14 @@ return(
 <div>
   <h1>Statistics</h1>
   <table>
+    <tbody>
   <StatisticLine text="good" value={props.left} />
   <StatisticLine text="neutral" value={props.middle} />
   <StatisticLine text="bad" value={props.right} />
   <StatisticLine text="all" value={addition} /> 
   <StatisticLine text="average" value={average} /> 
   <StatisticLine text="positive" value={positives} /> 
+    </tbody>
   </table>
   </div>
 )
@@ -58,20 +45,16 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [middle, setMiddle] = useState(0)
   const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
 
   const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
     setLeft(left + 1)
   }
 
   const handleMiddleClick = () => {
-    setAll(allClicks.concat('m'))
     setMiddle(middle + 1)
   }
 
   const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
     setRight(right + 1)
   }
 
@@ -82,8 +65,8 @@ const App = () => {
         <Button handleClick={handleLeftClick} text='Good'/>
         <Button handleClick={handleMiddleClick} text='Neutral'/>
         <Button handleClick={handleRightClick} text='Bad'/>
-        <History allClicks={allClicks} />
         <Statistics left={left} middle={middle} right={right} />
+
     </div>
   )
 }
